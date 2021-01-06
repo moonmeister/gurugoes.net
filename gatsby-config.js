f6523c2c6c359a1741bba81335870a75a34d6858
Alex Moon <alex.jared.moon@gmail.com>
2021-01-06 14:38:02 -0600

    update and cleanup config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,30 @@
+require("dotenv").config()
+
 module.exports = {
+  flags: {
+    FAST_DEV: true,
+    FAST_REFRESH: true,
+  },
   siteMetadata: {
-    title: "gurugoes.com",
+    title: "gurugoes.net",
   },
   plugins: [
     {
       resolve: "gatsby-source-wordpress-experimental",
       options: {
-        url: "https://wp.moonmeister.net/graphql",
+        url: "https://cms.gurugoes.net/graphql",
       },
     },
+    // {
+    //   resolve: "gatsby-source-strava",
+    //   options: {
+    //     stravaClientId: process.env.STRAVA_CLIENT_ID,
+    //     stravaClientSecret: process.env.STRAVA_CLIENT_SECRET,
+    //     stravaToken: process.env.STRAVA_TOKEN,
+    //   },
+    // },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     "gatsby-plugin-postcss",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
