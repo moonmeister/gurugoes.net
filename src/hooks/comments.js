73e7adc526f306abd47265678c5869340f48c8e3
Alex Moon <alex.jared.moon@gmail.com>
2021-04-12 09:09:22 -0500

    organize code

--- src/hooks/comments.js
+++ src/hooks/comments.js
@@ -1,47 +1,16 @@
-import { gql, GraphQLClient } from 'graphql-request'
 import { useQuery, useMutation } from "react-query"
 
-const client = new GraphQLClient("https://gurugoes.api.moonmeister.net/graphql", {
-  headers: {}
-})
+import { listCommentsByPostId, createComment } from "../libs/wpGraphqlClient"
 
 export function useComments(postId) {
   return useQuery(`comments-${postId}`, async () => {
-    const { post: { comments: { nodes } } } = await client.request(gql`
-      query postComments($postId: ID!){
-        post(id: $postId, idType: DATABASE_ID) {
-          comments {
-            nodes {
-              content
-              databaseId
-              parentDatabaseId
-              author {
-                node {
-                  name
-                }
-              }
-              dateGmt
-            }
-          }
-        }
-      }`, { postId })
-
-
+    const { post: { comments: { nodes } } } = await listCommentsByPostId(postId)
     return nodes
   }, { staleTime: 1000 * 60 * 2, cacheTime: 1000 * 60 * 30 })
 }
 
 export function useAddComment() {
-  return useMutation(async (data) => {
-    return client.request(gql`mutation postComment($name: String!, $email: String!, $comment: String!, $commentOn: Int!) {
-      createComment(input: {author: $name, authorEmail: $email, content: $comment, commentOn: $commentOn}) {
-        success
-        comment {
-          approved
-        }
-      }
-    }`, data)
-  }, {
+  return useMutation(async (data) => createComment(data), {
     onError(error) {
       console.error(error.response.errors[0].message)
     },
