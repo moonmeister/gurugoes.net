1e28b892df4f68607c0be674c51eb7ca69cff6a3
Alex Moon <moonmeister@users.noreply.github.com>
2025-04-02 17:20:59 -0700

    add netlify adapter

--- astro.config.js
+++ astro.config.js
@@ -5,21 +5,24 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
 
-	env: {
-		schema: {
-			WP_URL: envField.string({
-				context: "client",
-				access: "public",
-				optional: false,
-			}),
-		},
+  env: {
+      schema: {
+          WP_URL: envField.string({
+              context: "client",
+              access: "public",
+              optional: false,
+          }),
+      },
 	},
 
-	integrations: [react()],
+  integrations: [react()],
+  adapter: netlify(),
 });
