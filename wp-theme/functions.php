db045d7609411a5468b23d9b2741e3b603ad5ac4
Alex Moon <moonmeister@users.noreply.github.com>
2025-08-07 12:27:03 -0700

    fix: update them functions from server file

--- wp-theme/functions.php
+++ wp-theme/functions.php
@@ -1,4 +1,16 @@
 <?php
-  add_theme_support( 'post-thumbnails' );
+add_theme_support('post-thumbnails');
 
-?>
+add_filter('graphql_query_analyzer_get_headers', function ($headers) {
+	$headers['Surrogate-Key'] = $headers['X-GraphQL-Keys'];
+
+	unset($headers['X-GraphQL-Keys']);
+	return $headers;
+});
+
+add_filter('ure_supress_administrators_protection', function ($switch_off) {
+
+	$switch_off = true;
+
+	return $switch_off;
+}, 10, 1);
